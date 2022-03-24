768fa719c7e29d4b22feea9b0163f7d82049e17e
sarita711 <85009209+sarita711@users.noreply.github.com>
2022-03-24 21:01:49 +0530

    Create calculater.js

---
+++ calculater.js
@@ -0,0 +1,119 @@
+function getHistory() {
+    document.getElementById("history-value").innerText;
+
+}
+
+function printHistory(num) {
+
+    document.getElementById("history-value").innerText = num;
+
+}
+
+function getOutput() {
+
+    return document.getElementById("output-value").innerText;
+
+
+}
+
+function printOutput(num) {
+    if (num == "") {
+        document.getElementById("output-value").innerText = num;
+    } else {
+        document.getElementById("output-value").innerText = getFormatedNumber(num);
+    }
+}
+
+function getFormatedNumber(num) {
+    if (num == "-") {
+        return "";
+    }
+    var n = Number(num);
+    var value = n.toLocaleString("en");
+    return value;
+
+}
+
+function reverseNumberFormat(num) {
+
+    return Number(num.replace(/,/g, ''));
+
+
+}
+var operator = document.getElementsByClassName("operator");
+for (var i = 0; i < operator.length; ++i) {
+    operator[i].addEventListener('click', function() {
+
+        if (this.id == "clear") {
+            printHistory("");
+            printOutput("");
+        } else if (this.id == "backspace") {
+
+
+            var output = reverseNumberFormat(getOutput).toString();
+            if (output) {
+
+                output = output.substring(0, output.length - 1);
+
+            }
+
+        } else {
+            var output = getOutput();
+            var history = getHistory();
+            if (output == "" && history != "") {
+
+                if (isNaN(history[history.length - 1])) {
+                    history = history.substring(0, history.length - 1);
+                }
+            }
+            if (output != "" || history != "") {
+                output = output == "" ?
+                    output : reverseNumberFormat(output);
+                history = history.output();
+
+                if (this.id == "=") {
+
+                    printOutput(result);
+
+                    printHistory("");
+
+                } else {
+
+                    history = history + this.id;
+
+                    printHistory(history);
+                    printOutput("");
+                }
+
+            }
+
+        }
+
+
+    });
+
+}
+var number = document.getElementsByClassName("number");
+for (var i = 0; i < operator.length; ++i) {
+    operator[i].addEventListener('click', function() {
+        if (this.id == "clear") {
+            printHistory("");
+            printOutput("");
+        }
+
+
+
+    })
+}
+var number = document.getElementsByClassName("number");
+for (var i = 0; i < number.length; ++i) {
+    number[i].addEventListener('click', function() {
+        var output = reverseNumberFormat(getOutput());
+        if (output != NAN) {
+            output += output + this.id;
+            printOutput(output);
+        }
+
+    });
+
+}
